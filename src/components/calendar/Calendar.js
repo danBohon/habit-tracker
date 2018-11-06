67bcc069a20071a3f678822fac904f43e5709cbd
Daniel Bohon <dan@Daniels-MacBook-Pro.local>
2018-11-06 15:08:06 -0700

    user can check box on dashboard to save habit for the day

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -23,68 +23,8 @@ class Calendar extends Component {
     const { id } = this.props.location.state.habit;
     axios.post('/api/calendar', {habit_id: id} ).then((res) => this.setState( {calendar: res.data} )).then(() => this.initiateDays());
   }
-
-  initiateDays = () => {
-    const {calendar} = this.state;
-    // map over calendar to format days
-    const result = calendar.map((item, index) => moment(item.date));
-
-    // filter to only show previous days and today
-    const today = moment().add(5, 'd');
-    const beforeToday = (value) => moment(value).isSameOrBefore(today)
-    const filteredArr = result.filter(beforeToday)
-
-    // push to array with checked value from calendar
-    const weekArr = [];
-    for (let i = 0; i < filteredArr.length; i++) {
-      weekArr[i] = calendar[i]
-    } 
-    // console.log('weekArr', weekArr);
-
-    this.setState({daysArr: weekArr})
-
-    // const { habit } = this.props.location.state;
-    // // Set beginning and end of habit
-    // let startOfHabit = moment(habit.start_date);
-    // let endOfHabit = moment().add(habit.goal -1, 'day');
-    // let days = [];
-    // let day = startOfHabit;
-    
-    // create array of days for goal
-    // while (day <= endOfHabit) {
-    //   days.push(day);
-    //   day = day.clone().add(1, 'd');
-    // }
-    
-    // filter to only show habits before today
-    // const today = moment();
-    // const beforeToday = (value) => value.isSameOrBefore(today)
-    // const filteredArr = days.filter(beforeToday)
-    // this.setState({filteredArr: filteredArr})
-
-    // push all dates into object of arrays with checked boolean
-    // const weekArr = [];
-    // for (let i = 0; i < filteredArr.length; i++) {
-    //   weekArr[i] = {date: filteredArr[i], checked: false}
-    // }
-    // this.setState({daysArr: weekArr});
-  }
   
-  handleCheckChange = (index) => {
-    // change the value of checked in object when checkbox is checked
-    let newArr = this.state.daysArr
-    newArr[index].checked = !newArr[index].checked    
-    this.setState( {daysArr: newArr})
-
-    // do the same with calendar
-    // let newCal = this.state.calendar
-    // newCal[index].checked = newArr[index].checked
-    // this.setState( {calendar: newCal } )
-    // console.log('newCal', newCal);
-    console.log(this.state.daysArr[index]);
-    
-    axios.put('/api/calendar', {day: this.state.daysArr[index]}).then()
-    
+  countChecks = () => {
     // add to counter for each check
     let count = 0
     this.state.daysArr.map(
@@ -96,8 +36,39 @@ class Calendar extends Component {
       }
     )
     this.setState({ counter: count})
+  }
 
+  initiateDays = () => {
+    const {calendar} = this.state;
+    // map over calendar to format days
+    const result = calendar.map((item, index) => moment(item.date));
 
+    // filter to only show previous days and today
+    const today = moment();
+    const beforeToday = (value) => moment(value).isSameOrBefore(today)
+    const filteredArr = result.filter(beforeToday)
+
+    // push to array with checked value from calendar
+    const weekArr = [];
+    for (let i = 0; i < filteredArr.length; i++) {
+      weekArr[i] = calendar[i]
+    } 
+
+    this.setState({daysArr: weekArr})
+
+    this.countChecks();
+  }
+
+  
+  handleCheckChange = (index) => {
+    // change the value of checked in object when checkbox is checked
+    let newArr = this.state.daysArr
+    newArr[index].checked = !newArr[index].checked    
+    this.setState( {daysArr: newArr})
+    
+    axios.put('/api/calendar', {day: this.state.daysArr[index]}).then()
+
+    this.countChecks();
   }
   
   render() {
--- src/components/oneHabit/OneHabit.js
+++ src/components/oneHabit/OneHabit.js
@@ -3,19 +3,95 @@ import { Link } from "react-router-dom";
 import moment from 'moment';
 import { connect } from 'react-redux';
 import { updateHabit } from '../../ducks/reducer';
+import axios from 'axios';
 import './oneHabit.scss';
 
 class OneHabit extends Component {
+  constructor() {
+    super();
+    this.state = {
+      calendar: [],
+      counter: 0,
+      checkedToday: false
+    }
+  }
+
+  componentDidMount () {
+    this.getCalendar();
+    
+  }
+
+  getCalendar = () => {
+    const { id } = this.props.habit;
+    axios.post('/api/calendar', {habit_id: id} ).then((res) => this.setState( {calendar: res.data} )).then(() => this.countChecks()).then(() => this.isTodayChecked());
+  }
+
+  countChecks = () => {
+    let count = 0
+    this.state.calendar.map(
+      (day) => {
+        if (day.checked) {
+          count ++
+          return count;
+        } else return count;
+      }
+    )
+    this.setState({ counter: count})
+  }
+
+  isTodayChecked = () => {
+    const {calendar} = this.state;
+    // map over calendar to format days
+    const result = calendar.map((item) => moment(item.date));
+    // filter to only show today
+    const today = moment()
+    const isToday = (value) => moment(value).isSame(today, 'd');
+    const filteredArr = result.filter(isToday);
+    let todayDate = filteredArr[0].format();
+    
+    const newCal = calendar
+    for (let i = 0; i < calendar.length; i++) {
+      if (moment(newCal[i].date).isSame(todayDate, 'd')) {
+        this.setState({checkedToday: calendar[i].checked})
+      }
+    }
+  }
+
+  completeToday = () => {
+    const {calendar} = this.state;
+    // map over calendar to format days
+    const result = calendar.map((item) => moment(item.date));
+    // filter to only show today
+    const today = moment()
+    const isToday = (value) => moment(value).isSame(today, 'd');
+    const filteredArr = result.filter(isToday);
+    let todayDate = filteredArr[0].format();
+    
+    const newCal = calendar
+    for (let i = 0; i < calendar.length; i++) {
+      if (moment(newCal[i].date).isSame(todayDate, 'd')) {
+        newCal[i].checked = !newCal[i].checked
+        axios.put('/api/calendar', {day: newCal[i]}).then()
+        this.setState({checkedToday: calendar[i].checked})
+      }
+    }
+  }
+
+
   render(props) {
-    // console.log('onehabit props', this.props);
-    const checkedDays = [];
+    console.log('onehabit calendar', this.state.calendar);
     const { habit } = this.props;
     return (
-     <Link to={{pathname:`/calendar${habit.id}`, state: {habit, checkedDays}}} onClick={() => this.props.updateHabit(habit.goal, habit.start_date)}> <div className='card'>
+      <div>
+     <Link to={{pathname:`/calendar${habit.id}`, state: {habit}}} onClick={() => this.props.updateHabit(habit.goal, habit.start_date)}> <div className='card'>
         <h3>{habit.title}</h3>
         <div className='text'>Start Date: {moment(habit.start_date).format('LL')}</div>
+        <div>Complete: {this.state.counter} </div>
         <div>Goal: {habit.goal} </div>
       </div></Link>
+      <div>Complete habit for today:</div>
+      <input type="checkbox" checked={this.state.checkedToday} onChange={ () => this.completeToday()}></input>
+      </div>
     )
   }
 }
